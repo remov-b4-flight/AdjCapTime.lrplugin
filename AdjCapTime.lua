39d8532277533454c010b5d6124cc66264464e97
じゅん <jenoki48@gmail.com>
2020-04-19 15:26:34 +0900

    Use exiftool

--- AdjCapTime.lua
+++ AdjCapTime.lua
@@ -1,37 +1,78 @@
 --[[
-Info.lua
+AdjCapTime.lua
 AdjCapTime.lrplugin
 Author:@jenoki48
 ]]
 local LrApplication = import 'LrApplication'
 local LrTasks = import 'LrTasks'
 local LrDate = import 'LrDate'
+local LrDialog = import 'LrDialogs'
+local LrProgress = import 'LrProgressScope'
 local LrLogger = import 'LrLogger'
 Logger = LrLogger('AdjCapTime')
 Logger:enable('logfile')
 local CurrentCatalog = LrApplication.activeCatalog()
 local interval = 10
+local TagHead = 'wsl exiftool'
+local TagDateOpt = ' -allDates='
+local TagCapOpt = ' -Caption-Abstruct='
 
 function getIPTCDateTime(time)
 	return LrDate.timeToUserFormat(time,'%Y-%m-%dT%H:%M:%S',false)
 end
 
+function getExifToolDateTime(time)
+	return LrDate.timeToUserFormat(time,'%Y:%m:%d %H:%M:%S',false)
+end
+
+function getWSLPath(path)
+	local temp = string.gsub(path,'%a:\\',
+	function (s)
+		local t = string.gsub(s, ':', '', 1)
+		return '/mnt/'.. string.lower(t) 
+	end 
+	,1)
+	temp = string.gsub(temp, '\\', '/')
+	return temp
+end
+
 LrTasks.startAsyncTask( function ()
 	local TargetPhoto = CurrentCatalog:getTargetPhoto()
-	local TargetTime = TargetPhoto:getRawMetadata('dateTimeOriginal')
-	local IPTCDateTime = getIPTCDateTime(TargetTime)
-	Logger:info('Origin='.. IPTCDateTime)
-	
 	local SelectedPhotos = CurrentCatalog:getTargetPhotos()
+	local countPhotos = #SelectedPhotos
+	if (countPhotos == 0) then 
+		return 
+	end
+
+	local TargetTime = TargetPhoto:getRawMetadata('dateTimeOriginal')
+	if (TargetTime == nil) then 
+		TargetTime = TargetPhoto:getRawMetadata('dateTime')
+		if (TargetTime == nil) then 
+			LrDialog:message('Can\'t get target date/time.\nSet target date/time.')
+			return
+		end
+	end
+
+	local OriginDateTime = getExifToolDateTime(TargetTime)
+	local ProgressBar = LrProgress(
+		{title = 'Adjusting Times...'}
+	)
+	Logger:info('Origin='.. OriginDateTime)
 
 	CurrentCatalog:withWriteAccessDo('Set Capture Time',function()
 		for i,PhotoIt in ipairs(SelectedPhotos) do
 			if TargetPhoto.localIdentifier ~= PhotoIt.localIdentifier then
-				IPTCDateTime = getIPTCDateTime(TargetTime)
-				Logger:info(TargetTime ..'='.. IPTCDateTime)
-				PhotoIt:setRawMetadata('dateCreated',IPTCDateTime)
+				local FilePath = PhotoIt:getRawMetadata('path')
+				local WSLPath = getWSLPath(FilePath)
+				local DateTime = getExifToolDateTime(TargetTime)
+				local caption = PhotoIt:getFormattedMetadata('caption')
+				local CommandLine = TagHead .. TagCapOpt ..'\"'.. caption ..'\"'.. TagDateOpt ..'\"'.. DateTime ..'\" '.. WSLPath
+				Logger:info(CommandLine)
+				LrTasks.execute(CommandLine)
 			end
 			TargetTime = TargetTime + interval
+			ProgressBar:setPortionComplete(i,countPhotos)
 		end
+		ProgressBar:done()
 	end )
 end )
